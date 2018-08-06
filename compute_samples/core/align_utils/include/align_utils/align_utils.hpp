77070a366ed1c3df2d88ada65b7e43c831cf38e1
Lukasz Towarek <lukasz.towarek@intel.com>
2018-08-06 14:25:51 +0200

    Do not declare static functions in a header file
    Change-Id: I947ba29defbb1bd2cbf639653151ae32cfa4c3c4

--- compute_samples/core/align_utils/include/align_utils/align_utils.hpp
+++ compute_samples/core/align_utils/include/align_utils/align_utils.hpp
@@ -36,17 +36,17 @@ template <typename T>
 using PageAlignedVector =
     std::vector<T, boost::alignment::aligned_allocator<T, 4096>>;
 
-static uint32_t align(uint32_t value, uint32_t alignment) {
+inline uint32_t align(uint32_t value, uint32_t alignment) {
   return ((value + alignment - 1) / alignment) * alignment;
 }
 
-static uint32_t align16(uint32_t value) { return align(value, 16); }
+inline uint32_t align16(uint32_t value) { return align(value, 16); }
 
-static uint32_t align32(uint32_t value) { return align(value, 32); }
+inline uint32_t align32(uint32_t value) { return align(value, 32); }
 
-static uint32_t align64(uint32_t value) { return align(value, 64); }
+inline uint32_t align64(uint32_t value) { return align(value, 64); }
 
-static uint32_t align_units(uint32_t value, uint32_t alignment) {
+inline uint32_t align_units(uint32_t value, uint32_t alignment) {
   return align(value, alignment) / alignment;
 }
 }; // namespace align_utils
