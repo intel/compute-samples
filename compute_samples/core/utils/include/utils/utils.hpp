73b94816423ca484672c000dbb03fa83745a3a9f
Lukasz Towarek <lukasz.towarek@intel.com>
2019-08-20 12:12:53 +0200

    Add pack_vector function
    Change-Id: Ibb7cf71b704f884aaed9adfae47bb59013989dee

--- compute_samples/core/utils/include/utils/utils.hpp
+++ compute_samples/core/utils/include/utils/utils.hpp
@@ -25,6 +25,8 @@
 
 #include <vector>
 #include <string>
+#include <cassert>
+#include <cstring>
 
 namespace compute_samples {
 std::vector<uint8_t> load_binary_file(const std::string &file_path);
@@ -33,6 +35,38 @@ void save_binary_file(const std::vector<uint8_t> &data,
 
 template <typename T> int size_in_bytes(const std::vector<T> &v) {
   return static_cast<int>(sizeof(T) * v.size());
+}
+
+template <typename OUTPUT_TYPE, typename INPUT_TYPE>
+std::vector<OUTPUT_TYPE> pack_vector(const std::vector<INPUT_TYPE> &input,
+                                     const int step) {
+  static_assert(sizeof(OUTPUT_TYPE) >= sizeof(INPUT_TYPE),
+                "Size of OUTPUT_TYPE must be greater or equal to INPUT_TYPE");
+  assert(step > 0);
+
+  const size_t size_ratio = sizeof(OUTPUT_TYPE) / sizeof(INPUT_TYPE);
+
+  std::vector<OUTPUT_TYPE> output(input.size() / size_ratio);
+
+  const size_t columns = static_cast<size_t>(step);
+  const size_t rows = output.size() / columns;
+
+  for (size_t y = 0; y < rows; ++y) {
+    for (size_t x = 0; x < columns; ++x) {
+      std::vector<INPUT_TYPE> tmp(size_ratio);
+      for (size_t i = 0; i < tmp.size(); ++i) {
+        const size_t row_offset = y * columns * size_ratio;
+        const size_t column_offset = x;
+        const size_t element_offset = i * columns;
+        const size_t input_offset = row_offset + column_offset + element_offset;
+        tmp[i] = input[input_offset];
+      }
+      const size_t output_offset = y * columns + x;
+      std::memcpy(&output[output_offset], tmp.data(), sizeof(OUTPUT_TYPE));
+    }
+  }
+
+  return output;
 }
 
 } // namespace compute_samples
--- compute_samples/core/utils/test/utils_unit_tests.cpp
+++ compute_samples/core/utils/test/utils_unit_tests.cpp
@@ -20,8 +20,10 @@
  * SOFTWARE.
  */
 
-#include "utils/utils.hpp"
 #include "gtest/gtest.h"
+
+#include "utils/utils.hpp"
+namespace cs = compute_samples;
 
 template <typename T> class SizeInBytes : public testing::Test {};
 typedef testing::Types<uint8_t, int8_t, uint16_t, int16_t, uint32_t, int16_t,
@@ -31,17 +33,116 @@ TYPED_TEST_CASE(SizeInBytes, StandardTypes);
 
 TYPED_TEST(SizeInBytes, EmptyVector) {
   const std::vector<TypeParam> vector(0);
-  EXPECT_EQ(0, compute_samples::size_in_bytes(vector));
+  EXPECT_EQ(0, cs::size_in_bytes(vector));
 }
 
 TYPED_TEST(SizeInBytes, SingleElement) {
   const std::vector<TypeParam> vector(1);
-  EXPECT_EQ(sizeof(TypeParam) * vector.size(),
-            compute_samples::size_in_bytes(vector));
+  EXPECT_EQ(sizeof(TypeParam) * vector.size(), cs::size_in_bytes(vector));
 }
 
 TYPED_TEST(SizeInBytes, MultipleElements) {
   const std::vector<TypeParam> vector(2);
-  EXPECT_EQ(sizeof(TypeParam) * vector.size(),
-            compute_samples::size_in_bytes(vector));
+  EXPECT_EQ(sizeof(TypeParam) * vector.size(), cs::size_in_bytes(vector));
+}
+
+TEST(PackVector, Input8BitsOutput8BitsStep1) {
+  const std::vector<uint8_t> input = {0x00, 0x01, 0x02, 0x03};
+  const std::vector<uint8_t> expected = {0x00, 0x01, 0x02, 0x03};
+  const std::vector<uint8_t> output = cs::pack_vector<uint8_t>(input, 1);
+  EXPECT_EQ(expected, output);
+}
+
+TEST(PackVector, Input8BitsOutput16BitsStep1) {
+  const std::vector<uint8_t> input = {0x00, 0x01, 0x02, 0x03,
+                                      0x04, 0x05, 0x06, 0x07};
+  const std::vector<uint16_t> expected = {0x0100, 0x0302, 0x0504, 0x0706};
+  const std::vector<uint16_t> output = cs::pack_vector<uint16_t>(input, 1);
+  EXPECT_EQ(expected, output);
+}
+
+TEST(PackVector, Input8BitsOutput32BitsStep1) {
+  const std::vector<uint8_t> input = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05,
+                                      0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B,
+                                      0x0C, 0x0D, 0x0E, 0x0F};
+  const std::vector<uint32_t> expected = {0x03020100, 0x07060504, 0x0B0A0908,
+                                          0x0F0E0D0C};
+  const std::vector<uint32_t> output = cs::pack_vector<uint32_t>(input, 1);
+  EXPECT_EQ(expected, output);
+}
+
+TEST(PackVector, Input16BitsOutput16BitsStep1) {
+  const std::vector<uint16_t> input = {0x0100, 0x0302, 0x0504, 0x0706};
+  const std::vector<uint16_t> expected = {0x0100, 0x0302, 0x0504, 0x0706};
+  const std::vector<uint16_t> output = cs::pack_vector<uint16_t>(input, 1);
+  EXPECT_EQ(expected, output);
+}
+
+TEST(PackVector, Input16BitsOutput32BitsStep1) {
+  const std::vector<uint16_t> input = {0x0100, 0x0302, 0x0504, 0x0706,
+                                       0x0908, 0x0B0A, 0x0D0C, 0x0F0E};
+  const std::vector<uint32_t> expected = {0x03020100, 0x07060504, 0x0B0A0908,
+                                          0x0F0E0D0C};
+  const std::vector<uint32_t> output = cs::pack_vector<uint32_t>(input, 1);
+  EXPECT_EQ(expected, output);
+}
+
+TEST(PackVector, Input32BitsOutput32BitsStep1) {
+  const std::vector<uint32_t> input = {0x03020100, 0x07060504, 0x0B0A0908,
+                                       0x0F0E0D0C};
+  const std::vector<uint32_t> expected = {0x03020100, 0x07060504, 0x0B0A0908,
+                                          0x0F0E0D0C};
+  const std::vector<uint32_t> output = cs::pack_vector<uint32_t>(input, 1);
+  EXPECT_EQ(expected, output);
+}
+
+struct CustomType {
+  uint8_t s[3];
+};
+
+TEST(PackVector, Input8BitsOutput24BitsStep1) {
+  const std::vector<uint8_t> input = {0x00, 0x01, 0x02, 0x03, 0x04,
+                                      0x05, 0x06, 0x07, 0x08};
+  const std::vector<CustomType> expected = {
+      {0x00, 0x01, 0x02}, {0x03, 0x04, 0x05}, {0x06, 0x07, 0x08}};
+  const std::vector<CustomType> output = cs::pack_vector<CustomType>(input, 1);
+  for (size_t i = 0; i < output.size(); ++i) {
+    EXPECT_EQ(expected[i].s[0], output[i].s[0]);
+    EXPECT_EQ(expected[i].s[1], output[i].s[1]);
+    EXPECT_EQ(expected[i].s[2], output[i].s[2]);
+  }
+}
+
+TEST(PackVector, Input8BitsOutput8BitsStep2) {
+  const std::vector<uint8_t> input = {0x00, 0x01, 0x02, 0x03};
+  const std::vector<uint8_t> expected = {0x00, 0x01, 0x02, 0x03};
+  const std::vector<uint8_t> output = cs::pack_vector<uint8_t>(input, 2);
+  EXPECT_EQ(expected, output);
+}
+
+TEST(PackVector, Input8BitsOutput16BitsStep2) {
+  const std::vector<uint8_t> input = {0x00, 0x01, 0x02, 0x03,
+                                      0x04, 0x05, 0x06, 0x07};
+  const std::vector<uint16_t> expected = {0x0200, 0x0301, 0x0604, 0x0705};
+  const std::vector<uint16_t> output = cs::pack_vector<uint16_t>(input, 2);
+  EXPECT_EQ(expected, output);
+}
+
+TEST(PackVector, Input8BitsOutput32BitsStep2) {
+  const std::vector<uint8_t> input = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05,
+                                      0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B,
+                                      0x0C, 0x0D, 0x0E, 0x0F};
+  const std::vector<uint32_t> expected = {0x06040200, 0x07050301, 0x0E0C0A08,
+                                          0x0F0D0B09};
+  const std::vector<uint32_t> output = cs::pack_vector<uint32_t>(input, 2);
+  EXPECT_EQ(expected, output);
+}
+
+TEST(PackVector, Input8BitsOutput16BitsStep3) {
+  const std::vector<uint8_t> input = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05,
+                                      0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B};
+  const std::vector<uint16_t> expected = {0x0300, 0x0401, 0x0502,
+                                          0x0906, 0x0A07, 0xB08};
+  const std::vector<uint16_t> output = cs::pack_vector<uint16_t>(input, 3);
+  EXPECT_EQ(expected, output);
 }
