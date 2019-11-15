e2c9423cde96498b238e08b5d9cbd98319dc2e9c
Lukasz Towarek <lukasz.towarek@intel.com>
2019-11-15 09:07:13 +0100

    Remove GPU specific code from the application interface
    Change-Id: I87f481ca3a3342d262a028c07c73370f4abde8cb

--- compute_samples/core/application/src/application.cpp
+++ compute_samples/core/application/src/application.cpp
@@ -14,17 +14,6 @@ namespace compute_samples {
 Application::Status Application::run(std::vector<std::string> &command_line) {
   try {
     LOG_INFO << "Version: " << get_version_string();
-
-#ifndef _WIN32
-#define PUTENV putenv
-#else
-#define PUTENV _putenv
-#endif
-    char set_default_vendor[] = "BOOST_COMPUTE_DEFAULT_VENDOR=Intel";
-    char set_default_device[] = "BOOST_COMPUTE_DEFAULT_DEVICE_TYPE=GPU";
-    PUTENV(set_default_vendor);
-    PUTENV(set_default_device);
-
     return run_implementation(command_line);
   } catch (const std::exception &e) {
     LOG_FATAL << e.what();
