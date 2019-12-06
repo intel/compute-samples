2cd0d0d49fea106046cd851774057a782d3017f3
Lukasz Towarek <lukasz.towarek@intel.com>
2019-12-06 11:59:22 +0100

    Add installation script for VS 2019
    Change-Id: I63301ec94368936568608aebbf286044d5775491

---
+++ scripts/install/install_vs2019.ps1
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2019 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+#
+
+echo "Installing all dependencies"
+. "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+. "$PSScriptRoot/install_zlib"
+. "$PSScriptRoot/install_libpng"
+. "$PSScriptRoot/install_opencl"
+. "$PSScriptRoot/install_googletest"
+. "$PSScriptRoot/install_boost"
+. "$PSScriptRoot/download_mediadata"
