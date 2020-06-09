583afd9d9482f153fd92c8f075cc05372a016f09
Kacper Kasper <kacper.k.kasper@intel.com>
2020-06-09 11:06:16 +0000

    boost_intel: add mem queries to kernel class
    Change-Id: Ie4b3255183b1372abe00856df3fa4c43c5478bcb

--- compute_samples/core/boost_intel/include/boost/compute/intel/kernel.hpp
+++ compute_samples/core/boost_intel/include/boost/compute/intel/kernel.hpp
@@ -10,6 +10,9 @@
 
 #include <boost/compute/kernel.hpp>
 
+#include <boost/compute/device.hpp>
+#include <CL/cl_ext_intel.h>
+
 #include "ocl_entrypoints/cl_intel_unified_shared_memory.h"
 
 namespace boost {
@@ -21,6 +24,15 @@ class kernel_intel : public kernel {
 public:
   kernel_intel() : kernel() {}
   explicit kernel_intel(const kernel &other) : kernel(other) {}
+
+  const cl_ulong private_mem_size(const device &device) const {
+    return get_work_group_info<cl_ulong>(device, CL_KERNEL_PRIVATE_MEM_SIZE);
+  }
+
+  const cl_ulong spill_mem_size(const device &device) const {
+    return get_work_group_info<cl_ulong>(device,
+                                         CL_KERNEL_SPILL_MEM_SIZE_INTEL);
+  }
 
   void set_arg_mem_ptr(size_t index, void *ptr) {
     cl_int ret =
