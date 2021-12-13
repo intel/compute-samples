b2ef0f5fbeb7f09eb8bf7323b9d99eec84c18e93
Slawomir Makowski <slawomir.makowski@intel.com>
2021-12-13 10:00:29 +0100

    Update level zero loader to 1.6.2 version

--- scripts/install/install_level_zero.ps1
+++ scripts/install/install_level_zero.ps1
@@ -5,10 +5,10 @@
 #
 
 echo "Downloading Level Zero"
-wget -Uri https://github.com/oneapi-src/level-zero/archive/v1.0.26.zip -OutFile level-zero-1.0.26.zip
+wget -Uri https://github.com/oneapi-src/level-zero/archive/v1.6.2.zip -OutFile level-zero-1.6.2.zip
 
 echo "Extracting Level Zero"
-. "C:\Program Files\7-Zip\7z.exe" x level-zero-1.0.26.zip
+. "C:\Program Files\7-Zip\7z.exe" x level-zero-1.6.2.zip
 
 echo "Installing Level Zero"
 $rootPath = (Resolve-Path ".").ToString()
@@ -17,7 +17,7 @@ mkdir $installPath -Force
 $levelZeroPath = $installPath + "/level_zero"
 
 pushd
-cd level-zero-1.0.26
+cd level-zero-1.6.2
 mkdir build | Out-Null
 cd build
 
@@ -26,5 +26,5 @@ cmake --build . --target INSTALL --config Release
 popd
 
 echo "Cleaning Level Zero"
-rm -R level-zero-1.0.26
-rm level-zero-1.0.26.zip
+rm -R level-zero-1.6.2
+rm level-zero-1.6.2.zip
--- scripts/install/install_level_zero.sh
+++ scripts/install/install_level_zero.sh
@@ -7,10 +7,10 @@
 #
 
 echo "Downloading Level Zero"
-wget https://github.com/oneapi-src/level-zero/archive/v1.0.26.zip -O level-zero-1.0.26.zip
+wget https://github.com/oneapi-src/level-zero/archive/v1.6.2.zip -O level-zero-1.6.2.zip
 
 echo "Extracting Level Zero"
-unzip -q level-zero-1.0.26.zip
+unzip -q level-zero-1.6.2.zip
 
 echo "Installing Level Zero"
 ROOT_PATH=`pwd`
@@ -18,7 +18,7 @@ INSTALL_PATH="$ROOT_PATH/third_party"
 LEVEL_ZERO_PATH="$INSTALL_PATH/level_zero"
 
 pushd .
-cd level-zero-1.0.26
+cd level-zero-1.6.2
 mkdir build
 cd build
 
@@ -27,5 +27,5 @@ cmake --build . --target install
 popd
 
 echo "Cleaning Level Zero"
-rm -rf level-zero-1.0.26
-rm level-zero-1.0.26.zip
+rm -rf level-zero-1.6.2
+rm level-zero-1.6.2.zip
