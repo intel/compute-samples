5fc8090e00007b6ae9996b086422ee26d173066d
Jaroslaw Warchulski <jaroslaw.warchulski@intel.com>
2025-05-07 12:56:31 +0200

    Update zlib to 1.3.1
    Signed-off-by: Jaroslaw Warchulski <jaroslaw.warchulski@intel.com>

--- scripts/install/install_zlib.ps1
+++ scripts/install/install_zlib.ps1
@@ -1,14 +1,14 @@
 #
-# Copyright (C) 2019 Intel Corporation
+# Copyright (C) 2019-2025 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 #
 
 echo "Downloading zlib"
-wget -Uri https://zlib.net/zlib1213.zip -OutFile zlib1213.zip
+wget -Uri https://zlib.net/zlib131.zip -OutFile zlib131.zip
 
 echo "Extracting zlib"
-. "C:\Program Files\7-Zip\7z.exe" x zlib1213.zip
+. "C:\Program Files\7-Zip\7z.exe" x zlib131.zip
 
 echo "Installing zlib"
 $rootPath = (Resolve-Path ".").ToString()
@@ -17,7 +17,7 @@ mkdir $installPath -Force
 $zlibPath = $installPath + "/zlib"
 
 pushd
-cd zlib-1.2.13
+cd zlib-1.3.1
 mkdir build | Out-Null
 cd build
 
@@ -26,5 +26,5 @@ cmake --build . --target INSTALL --config Release
 popd
 
 echo "Cleaning zlib"
-rm -R zlib-1.2.13
-rm zlib1213.zip
+rm -R zlib-1.3.1
+rm zlib131.zip
